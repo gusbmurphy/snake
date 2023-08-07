25222148015ed9d34cd0172117b5fea44ecef5c1
Augustus Murphy <gusbmurphy@gmail.com>
2023-08-06 22:42:25 -0400

    Add basic menus and with modes

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,70 @@
 use bracket_lib::prelude::*;
 
-struct State {}
+enum GameMode {
+    Menu,
+    Playing,
+    GameOver,
+}
+
+struct State {
+    mode: GameMode,
+    score: i32,
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
+        match self.mode {
+            GameMode::GameOver => self.game_over(ctx),
+            GameMode::Playing => self.game_over(ctx),
+            GameMode::Menu => self.main_menu(ctx),
+        }
+    }
+}
+
+impl State {
+    fn new() -> Self {
+        State {
+            mode: GameMode::Menu,
+            score: 0,
+        }
+    }
+
+    fn watch_for_start_or_quit(&mut self, ctx: &mut BTerm) {
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+
+    fn restart(&mut self) {
+        self.mode = GameMode::Menu;
+    }
+
+    fn main_menu(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        ctx.print(1, 1, "Hell world!");
+        ctx.print_centered(5, "Welcome to Snake");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        self.watch_for_start_or_quit(ctx);
+    }
+
+    fn game_over(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "You are dead!");
+        ctx.print_centered(6, &format!("You earned {} points", self.score));
+        ctx.print_centered(8, "(P) Play Again");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        self.watch_for_start_or_quit(ctx);
     }
 }
 
 fn main() -> BError {
     let context = BTermBuilder::simple80x50().with_title("Snake").build()?;
 
-    main_loop(context, State {})
+    main_loop(context, State::new())
 }
