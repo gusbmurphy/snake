0a6266cd61156b0a8e44f8e54ba185be1853a782
Augustus Murphy <gusbmurphy@gmail.com>
2023-08-20 17:22:30 -0500

    Add Apple and render on screen

--- src/state.rs
+++ src/state.rs
@@ -1,6 +1,8 @@
 use crate::prelude::*;
 
 const FRAME_DURATION: f32 = 75.0;
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
 
 enum GameMode {
     Menu,
@@ -8,11 +10,30 @@ enum GameMode {
     GameOver,
 }
 
+struct Apple {
+    x_position: i32,
+    y_position: i32,
+}
+
+impl Apple {
+    fn new(x_position: i32, y_position: i32) -> Self {
+        Apple {
+            x_position,
+            y_position,
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(self.x_position, self.y_position, RED, BLACK, to_cp437('▲'))
+    }
+}
+
 pub struct State {
     mode: GameMode,
     score: i32,
     frame_time: f32,
     player: Player,
+    apple: Apple,
 }
 
 impl GameState for State {
@@ -32,6 +53,7 @@ impl State {
             score: 0,
             frame_time: 0.0,
             player: Player::new(10, 10),
+            apple: Apple::new(20, 20),
         }
     }
 
@@ -81,6 +103,7 @@ impl State {
             self.player.move_forward();
         }
 
+        self.apple.render(ctx);
         self.player.render(ctx);
     }
 }
