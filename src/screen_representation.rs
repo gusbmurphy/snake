fb72ac9ec45bb0059a21da0dfd6c8e19024d443f
Augustus Murphy <gusbmurphy@gmail.com>
2023-09-02 16:01:11 -0500

    Add missing methods to ScreenRepresentation

--- src/screen_representation.rs
+++ src/screen_representation.rs
@@ -15,7 +15,25 @@ impl ScreenRepresentation {
             x_position: position.get_x_position(),
             y_position: position.get_y_position(),
         }
-    }   
+    }
+
+    pub fn get_glyph(&self) -> FontCharType {
+        return self.glyph;
+    }
+
+    pub fn get_color(&self) -> (u8, u8, u8) {
+        return self.color;
+    }
+}
+
+impl Position for ScreenRepresentation {
+    fn get_x_position(&self) -> i32 {
+        return self.x_position;
+    }
+
+    fn get_y_position(&self) -> i32 {
+        return self.y_position;
+    }
 }
 
 pub trait ScreenRepresentable {
